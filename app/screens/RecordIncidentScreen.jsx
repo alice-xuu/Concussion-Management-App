643a6d93998ecc87185d5d4a3305bb32d49b0ca9
Crystal Chan <ccha8970@uni.sydney.edu.au>
2021-08-24 19:05:04 +1000

    RecordIncidentScreen added home button

--- app/screens/RecordIncidentScreen.jsx
+++ app/screens/RecordIncidentScreen.jsx
@@ -1,19 +1,40 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Pressable } from 'react-native';
 
 /**
  * Asks user for details about the concussion and gives a suggestion based on
  * the user's responses.
  */
-function RecordIncidentScreen() {
+function RecordIncidentScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>Record Incident Screen</Text>
+      <Pressable style={styles.button} onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.text}>Home</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 10,
+    elevation: 3,
+    backgroundColor: 'black',
+    margin: 30,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
   container: {
     flex: 1,
     alignItems: 'center',
