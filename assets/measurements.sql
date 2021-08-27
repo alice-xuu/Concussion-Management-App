fa31e465f1a55c340059f1b84e4ddb39f015ea05
Joji Tenges <jten4148@uni.sydney.edu.au>
2021-08-27 22:36:24 +1000

    add .sql and .db to .gitignore
    * remove measurements.sql

--- assets/measurements.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Patient (
-    patient_id INTEGER PRIMARY KEY,
-    first_name VARCHAR(200),
-    last_name VARCHAR(200)
-);
-
--- Enforce foreign keys, which are disabled by default
-PRAGMA foreign_keys = ON;
