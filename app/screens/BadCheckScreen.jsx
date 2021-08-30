1ba031828b7c8f7a178f6bf3c8a6f4c1c674d46f
Crystal Chan <ccha8970@uni.sydney.edu.au>
2021-08-30 11:59:59 +1000

    resolve conflict with BadCheckScreen

--- app/screens/BadCheckScreen.jsx
+++ app/screens/BadCheckScreen.jsx
@@ -12,11 +12,30 @@ function BadCheckScreen({ navigation }) {
       <Text style={styles.text}>
         Your patient needs to go to hospital immediately, please call 000 now.
       </Text>
+      <Pressable disable={true} style={styles.callSymbol}>
+        <Text style={styles.label}>Call 000</Text>
+      </Pressable>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate('Create Profile')}
+      >
+        <Text style={styles.label}>Next</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  callSymbol: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 100,
+    backgroundColor: 'red',
+    marginHorizontal: 50,
+    marginVertical: 10,
+  },
   button: {
     alignItems: 'center',
     alignSelf: 'stretch',
