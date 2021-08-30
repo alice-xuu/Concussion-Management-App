b019837d983e95428208711a1a27f6a4640fa7bb
Crystal Chan <ccha8970@uni.sydney.edu.au>
2021-08-30 11:54:48 +1000

    resolve conflict with BadCheckScreen

--- app/screens/BadCheckScreen.jsx
+++ app/screens/BadCheckScreen.jsx
@@ -12,9 +12,6 @@ function BadCheckScreen({ navigation }) {
       <Text style={styles.text}>
         Your patient needs to go to hospital immediately, please call 000 now.
       </Text>
-      <Pressable style={styles.button} onPress={() => navigation.goBack()}>
-        <Text style={styles.label}>Back</Text>
-      </Pressable>
       <Pressable
         style={styles.button}
         onPress={() => navigation.navigate('Home')}
