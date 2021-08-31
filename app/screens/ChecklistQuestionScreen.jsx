253edf070696ab3634f5fd61cc7a8f2ce57fe9f4
Kurt Lu <falu5003@uni.sydney.edu.au>
2021-08-31 16:47:29 +1000

    added the rest of the questions

--- app/screens/ChecklistQuestionScreen.jsx
+++ app/screens/ChecklistQuestionScreen.jsx
@@ -8,6 +8,16 @@ import { useState } from 'react';
  */
 function CheckListQuestionScreen({ navigation }) {
   const [toggleCheckBox1, setToggleCheckBox1] = useState(false);
+  const [toggleCheckBox2, setToggleCheckBox2] = useState(false);
+  const [toggleCheckBox3, setToggleCheckBox3] = useState(false);
+  const [toggleCheckBox4, setToggleCheckBox4] = useState(false);
+  const [toggleCheckBox5, setToggleCheckBox5] = useState(false);
+  const [toggleCheckBox6, setToggleCheckBox6] = useState(false);
+  const [toggleCheckBox7, setToggleCheckBox7] = useState(false);
+  const [toggleCheckBox8, setToggleCheckBox8] = useState(false);
+  const [toggleCheckBox9, setToggleCheckBox9] = useState(false);
+  const [toggleCheckBox10, setToggleCheckBox10] = useState(false);
+
   return (
     <View style={styles.container}>
       <Text>
@@ -23,8 +33,84 @@ function CheckListQuestionScreen({ navigation }) {
         />
         <Text> Neck pain or tenderness</Text>
       </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox2}
+          onValueChange={(newValue) => setToggleCheckBox2(newValue)}
+        />
+        <Text> Double vision</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox3}
+          onValueChange={(newValue) => setToggleCheckBox3(newValue)}
+        />
+        <Text> Weakness or tingling/burning in the arms or legs</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox4}
+          onValueChange={(newValue) => setToggleCheckBox4(newValue)}
+        />
+        <Text> Severe or increasing headache</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox5}
+          onValueChange={(newValue) => setToggleCheckBox5(newValue)}
+        />
+        <Text> Seizures or convulsions</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox6}
+          onValueChange={(newValue) => setToggleCheckBox6(newValue)}
+        />
+        <Text> Loss of consciousness</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox7}
+          onValueChange={(newValue) => setToggleCheckBox7(newValue)}
+        />
+        <Text> Deteriorating conscious state</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox8}
+          onValueChange={(newValue) => setToggleCheckBox8(newValue)}
+        />
+        <Text> Vomiting</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox9}
+          onValueChange={(newValue) => setToggleCheckBox9(newValue)}
+        />
+        <Text> Increasing restlessness</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox10}
+          onValueChange={(newValue) => setToggleCheckBox10(newValue)}
+        />
+        <Text> Agitation or combativeness</Text>
+      </View>
       <Text>
-        Do you have the first symptom?: {toggleCheckBox1 ? '✅' : '❌'}
+        Do you have the first symptom(testing)?: {toggleCheckBox1 ? '✅' : '❌'}
+      </Text>
+      <Text>
+        Do you have the second symptom(testing)?:{' '}
+        {toggleCheckBox2 ? '✅' : '❌'}
       </Text>
       <Pressable
         style={styles.button}
