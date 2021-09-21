9dffb21197d350ea147720c4fff7815432397768
Alice Xu <alxu2606@uni.sydney.edu.au>
2021-09-21 20:58:11 +1000

    transitions for further tests

--- app/screens/ReactionTests/RTThree.jsx
+++ app/screens/ReactionTests/RTThree.jsx
@@ -35,7 +35,7 @@ export default function RTThree({ navigation }) {
       </Text>
 
       <TouchableOpacity
-        onPress={() => navigation.navigate('Reaction Test 1')}
+        onPress={() => navigation.navigate('Balance Test 1')}
         style={uiStyle.bottomButton}
       >
         <Text style={uiStyle.buttonLabel}>Start!</Text>
