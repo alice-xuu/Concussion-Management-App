f3910e63fe9e7360d7848c987423a89ad4590060
Patrick Wong <pwon7053@uni.sydney.edu.au>
2021-10-12 17:47:50 +1100

    UI for row 2 completed

--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -53,6 +53,10 @@ function HomeScreen({ navigation }) {
         {/*<Button style={styles.startCheckText} {'Start Check'} onPress={createAlert} />*/}
       </View>
 
+      <TouchableOpacity onPress={() => navigation.navigate('Voms Test S 1')}>
+        <Text>VOMS Row 2</Text>
+      </TouchableOpacity>
+
       <View style={styles.container2}>
         <TouchableOpacity
           onPress={() => navigation.navigate('Documents')}
@@ -61,6 +65,7 @@ function HomeScreen({ navigation }) {
           <Text style={uiStyle.buttonLabel}>View History</Text>
         </TouchableOpacity>
       </View>
+
     </SafeAreaView>
   );
 }
---
+++ app/screens/VomsTests/Row 2/VomsS1.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Text, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
+
+import uiStyle from '../../../components/uiStyle';
+
+function VomsS1({ navigation }) {
+  return (
+    <SafeAreaView style={uiStyle.container}>
+      <Text style={uiStyle.titleText}>Saccades</Text>
+      <ScrollView>
+        <Text style={uiStyle.text}>
+          The affected person will be shown two circles at either end of the
+          screen. Ask them to alternate looking at each circle.
+          {'\n'} {'\n'}
+          Please allow them to sit down and place the phone landscape at eye
+          level, ___cm away.
+        </Text>
+      </ScrollView>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Voms Test SP 3')}
+        style={uiStyle.bottomButton}
+      >
+        <Text style={uiStyle.buttonLabel}>Next</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+export default VomsS1;
