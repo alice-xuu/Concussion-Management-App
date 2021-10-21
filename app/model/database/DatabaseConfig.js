15c283fe1c88e2d5607e94be1677b3a0a1a72ed4
Joji Tenges <jten4148@uni.sydney.edu.au>
2021-10-21 12:58:25 +1100

    SM0G-132 bugfix and cleanup
    * add missing commas in DatabaseConfig.js
    * remove console.log from VomsInitialSymptoms.jsx

--- app/model/database/DatabaseConfig.js
+++ app/model/database/DatabaseConfig.js
@@ -83,12 +83,12 @@ CREATE TABLE IF NOT EXISTS ReactionTest (
   // User responses for symptom check after each VOMS test section
   `
 CREATE TABLE IF NOT EXISTS VOMSSymptoms (
-    vomsSymptoms_id INTEGER PRIMARY KEY
+    vomsSymptoms_id INTEGER PRIMARY KEY,
     report_id INTEGER REFERENCES IncidentReport(report_id),
-    description VARCHAR(100)
-    headache_rating INTEGER CHECK(headache_rating >= 0 and headache_rating <= 10)
-    nausea_rating INTEGER CHECK(nausea_rating >= 0 and nausea_rating <= 10)
-    dizziness_rating INTEGER CHECK(dizziness_rating >= 0 and dizziness_rating <= 10)
+    description VARCHAR(100),
+    headache_rating INTEGER CHECK(headache_rating >= 0 and headache_rating <= 10),
+    nausea_rating INTEGER CHECK(nausea_rating >= 0 and nausea_rating <= 10),
+    dizziness_rating INTEGER CHECK(dizziness_rating >= 0 and dizziness_rating <= 10),
     fogginess_rating INTEGER CHECK(fogginess_rating >= 0 and fogginess_rating <= 10)
 );
 `,
