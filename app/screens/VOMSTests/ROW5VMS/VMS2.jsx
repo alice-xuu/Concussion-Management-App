95c33439a8c4b36d99ba86f7e117522e4f587e9a
Alice Xu <alxu2606@uni.sydney.edu.au>
2021-11-01 19:33:31 +1100

    beep stops on goback/next

--- app/screens/VOMSTests/ROW5VMS/VMS2.jsx
+++ app/screens/VOMSTests/ROW5VMS/VMS2.jsx
@@ -8,16 +8,21 @@ import {
 } from 'react-native';
 import uiStyle from '../../../components/uiStyle';
 import { Audio } from 'expo-av';
+import { useEffect } from 'react';
 
 function VMS2(props) {
   const [sound, setSound] = React.useState();
   const [playing, setPlaying] = React.useState(false);
+  const [repetition, setRepetition] = React.useState(20);
+  const [intervalId, setIntervalId] = React.useState(null);
+
   async function playSound() {
     const { sound } = await Audio.Sound.createAsync(
       require('../../../../assets/beep.mp3'),
     );
     setSound(sound);
     await sound.playAsync();
+    setRepetition(repetition - 1);
   }
 
   React.useEffect(() => {
@@ -28,7 +33,7 @@ function VMS2(props) {
       : undefined;
   }, [sound]);
 
-  function playRepetitions() {
+  /*  function playRepetitions() {
     for (let i = 0; i < 20; i++) {
       setTimeout(() => {
         playSound().then();
@@ -37,9 +42,36 @@ function VMS2(props) {
   }
 
   if (!playing) {
-    setPlaying(true);
-    playRepetitions();
-  }
+    setTimeout(() => {
+      setPlaying(true);
+      playRepetitions();
+    }, 1200);
+  }*/
+
+  useEffect(() => {
+    if (repetition < 0) {
+      return;
+    }
+
+    const id = setInterval(function () {
+      playSound().then();
+      console.log(repetition);
+    }, 1200);
+    setIntervalId(id);
+    return () => clearInterval(id);
+  }, [repetition]);
+
+  /*setPlaying(true);
+    var intevID = setInterval(function () {
+      if (repetition === 20) {
+      } else {
+        playSound().then();
+        setRepetition((repetition) => repetition + 1);
+      }
+      console.log(repetition);
+    }, 1200);
+    setIntervalId(intevID);
+  }*/
 
   return (
     <SafeAreaView style={uiStyle.container}>
@@ -48,6 +80,7 @@ function VMS2(props) {
       </View>
       <TouchableOpacity
         onPress={() => {
+          window.clearInterval(intervalId);
           props.navigation.navigate('VOMS VMS 3 Response 8');
         }}
         style={uiStyle.bottomButton}
