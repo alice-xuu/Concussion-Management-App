a27259247f5859e1cae879a6fb940195377418ae
Alice Xu <alxu2606@uni.sydney.edu.au>
2021-11-03 00:09:53 +1100

    added screen touch to RTTwo

--- app/screens/VOMSTests/Row1SmoothPursuits/SP2.jsx
+++ app/screens/VOMSTests/Row1SmoothPursuits/SP2.jsx
@@ -7,7 +7,7 @@ import {
   View,
   Text,
   Dimensions,
-} from "react-native";
+} from 'react-native';
 import uiStyle from '../../../components/uiStyle';
 import { useEffect, useRef } from 'react';
 
