2d475976010c2f6e9be68d08e4fcb2697b1e4938
Alice Xu <alxu2606@uni.sydney.edu.au>
2021-11-03 00:37:46 +1100

    move s2 dots to edges of screen

--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -91,12 +91,12 @@ function HomeScreen({ navigation }) {
       {/*>*/}
       {/*  <Text style={uiStyle.buttonLabel}>sample database</Text>*/}
       {/*</TouchableOpacity>*/}
-      {/*<TouchableOpacity*/}
-      {/*  onPress={() => navigation.navigate('VOMS Start')}*/}
-      {/*  style={uiStyle.bottomButton}*/}
-      {/*>*/}
-      {/*  <Text style={uiStyle.buttonLabel}>Voms Start</Text>*/}
-      {/*</TouchableOpacity>*/}
+      <TouchableOpacity
+        onPress={() => navigation.navigate('VOMS Saccades 1')}
+        style={uiStyle.bottomButton}
+      >
+        <Text style={uiStyle.buttonLabel}>Voms Start</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
